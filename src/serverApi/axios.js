3e414a9b8e45614c2a00ee2ccb7b76f2f738924a
dwg21 <95627833+dwg21@users.noreply.github.com>
2023-04-08 12:51:03 -0500

    ready for deployment .1

--- src/serverApi/axios.js
+++ src/serverApi/axios.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 axios.defaults.withCredentials = true
 
 const ServerApi = axios.create({
-    // baseURL:'https://espanolmaestroserver.onrender.com',
-    baseURL: "http://localhost:3005",
+    baseURL:'https://espanolmaestroserver.onrender.com',
     withCredentials: true,
 
 })
 
 export default ServerApi
+
